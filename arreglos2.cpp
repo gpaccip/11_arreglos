bed0ccd175175a121b8276a858638f5a7e17f251
gpaccip <gpaccip@unjbg.edu.pe>
2024-07-01 11:19:00 -0500

    Añadiendo arreglos2.cpp

---
+++ arreglos2.cpp
@@ -0,0 +1,31 @@
+//arreglos2.cpp
+//Giorgini Disalvatore Pacci Perez
+#include<iostream>
+
+using namespace std;
+int main(){
+	int n,a;
+	int impar[100], par[100];
+	cout<<"Digite un maximo de elementos";cin>>n;
+	
+	for(int i=0; i<=n; i++){
+		if(i%2==0){
+			impar[i]=a;
+			
+	cout<<endl;cin>>impar[i];
+			
+		}
+		else{
+		
+			impar[i]=a;
+			
+	cout<<endl;cin>>par[i];
+	
+			
+		}
+		
+	}
+	 
+	
+	return 0;
+}
