44ac131e664f6f4f552cdc7f514834667b27c87f
gpaccip <gpaccip@unjbg.edu.pe>
2024-07-07 20:56:54 -0500

    Añadiendo arreglos2.cpp

--- arreglos2.cpp
+++ arreglos2.cpp
@@ -4,28 +4,30 @@
 
 using namespace std;
 int main(){
-	int n,a;
-	int impar[100], par[100];
-	cout<<"Digite un maximo de elementos";cin>>n;
-	
-	for(int i=0; i<=n; i++){
-		if(i%2==0){
-			impar[i]=a;
-			
-	cout<<endl;cin>>impar[i];
-			
+	int n,j=0,k=0;
+	int impar[40], par[40], num[10];
+		cout<<"Digite un maximo de elementos: ";cin>>n;
+		
+	for(int i=0; i<n; i++){
+			cout<<"Digite el numero "<<i+1<<": ";cin>>num[i];
+		if(num[i]%2==0){
+			par[j]=num[i];
+				j++;
 		}
 		else{
-		
-			impar[i]=a;
-			
-	cout<<endl;cin>>par[i];
-	
-			
+			impar[k]=num[i];
+				k++;
 		}
-		
 	}
-	 
+	cout<<"Los numeros pares son: "<<endl;
+	for(int i=0; i<j; i++){
 	
+		cout<<par[i]<<endl;
+	}
+	cout<<"Los numeros impares son: "<<endl;
+	for(int i=0; i<k; i++){
+	
+		cout<<impar[i]<<endl;
+	}
 	return 0;
 }
